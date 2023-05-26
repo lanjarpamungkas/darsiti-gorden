a64adb17cff4ecaf818b81bd5a9cb78beb6944de
lanjarpamungkas <lanjar.raharjo@gmail.com>
2023-05-26 08:59:58 +0700

    membuat websiteelegante-coffee

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika humberger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar buat menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
